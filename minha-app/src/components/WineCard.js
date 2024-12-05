ab3028cfbb0740ac25264d2c72ab9a07b01da13b
Correiiaa <84743885+Correiiaa@users.noreply.github.com>
2024-12-05 20:31:25 +0000

    do more

--- minha-app/src/components/WineCard.js
+++ minha-app/src/components/WineCard.js
@@ -5,7 +5,7 @@ function WineCard({ wine }) {
   return (
     <div className="wine-card">
       <h2>{wine.name}</h2>
-      <img src={wine.image} alt={wine.name} />        
+      <img src={wine.image} alt='vinho'/>        
       <p>Preço: €{wine.price}</p>
       <p>Estoque: {wine.stock}</p>    
       <button disabled={wine.stock === 0}>Adicionar ao Carrinho</button>
--- minha-app/src/data/wines.js
+++ minha-app/src/data/wines.js
@@ -1,8 +1,8 @@
 // src/data/wines.js
 const wines = [
-    { id: 1, name: 'Encosta do Sobral Reserva Tinto 2020', price: 20, stock: 10, image:'./img/Encosta-do-Sobral-Reserva-Tinto.png'},
-    { id: 2, name: 'Perspectiva Douro Doc Branco 2023', price: 15, stock: 5, image:'./img/Perspectiva-Colheita-Branco.png'},
-    { id: 3, name: 'Vinho Rosé C', price: 18, stock: 0 },
+    { id: 1, name: 'Encosta do Sobral Reserva Tinto 2020', price: 20, stock: 10, image:'https://tesourosdebaco.com/wp-content/uploads/2024/11/Encosta-do-Sobral-Reserva-Tinto.png'},
+    { id: 2, name: 'Perspectiva Douro Doc Branco 2023', price: 15, stock: 5, image:'https://tesourosdebaco.com/wp-content/uploads/2024/11/Perspectiva-Colheita-Branco.png'},
+    { id: 3, name: 'Espumante M&m Blossom Rose', price: 18, stock: 0},
     { id: 4, name: 'Vinho Tinto D', price: 22, stock: 0 },
     // Adicione mais vinhos conforme necessário
   ];
